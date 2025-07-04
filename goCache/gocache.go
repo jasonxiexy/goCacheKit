662275967b77d0b5494fc8f84b359b465f9c138f
Xiaoyang(Jason) Xie <xjason0312@gmail.com>
2025-07-04 17:28:31 -0400

    added distributed nodes

--- goCache/gocache.go
+++ goCache/gocache.go
@@ -24,6 +24,7 @@ type Group struct {
 	name      string
 	getter    Getter
 	mainCache cache
+	peers     PeerPicker
 }
 
 // global variables
@@ -72,6 +73,16 @@ func (g *Group) Get(key string) (Byteview, error) {
 
 // if key not exist in cache, then load would use getLocally to callback data
 func (g *Group) load(key string) (value Byteview, err error) {
+	// 使用 PickPeer() 方法选择节点，若非本机节点，则调用 getFromPeer() 从远程获取。若是本机节点或失败，则回退到 getLocally()。
+	if g.peers != nil {
+		if peer, ok := g.peers.PickPeer(key); ok {
+			if value, err = g.getFromPeer(peer, key); err == nil {
+				return value, nil
+			}
+			log.Println("[GoCache] Failed to get from peer", err)
+		}
+	}
+
 	return g.getLocally(key)
 }
 
@@ -90,4 +101,22 @@ func (g *Group) getLocally(key string) (Byteview, error) {
 // the data from callback will be added back to the lru cache
 func (g *Group) populateCache(key string, value Byteview) {
 	g.mainCache.add(key, value)
+}
+
+// RegisterPeers registers a PeerPicker for choosing remote peer
+func (g *Group) RegisterPeers(peers PeerPicker) {
+	// 新增 RegisterPeers() 方法，将 实现了 PeerPicker 接口的 HTTPPool 注入到 Group 中。
+	if g.peers != nil {
+		panic("RegisterPeerPicker called more than once")
+	}
+	g.peers = peers
+}
+
+func (g *Group) getFromPeer(peer PeerGetter, key string) (Byteview, error) {
+	// 新增 getFromPeer() 方法，使用实现了 PeerGetter 接口的 httpGetter 从访问远程节点，获取缓存值。
+	bytes, err := peer.Get(g.name, key)
+	if err != nil {
+		return Byteview{}, err
+	}
+	return Byteview{b: bytes}, nil
 }
--- goCache/http.go
+++ goCache/http.go
@@ -2,18 +2,29 @@ package gocache
 
 import (
 	"fmt"
+	"gocache/consistenthash"
+	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
+	"sync"
 )
 
-const defaultBasePath = "/_gocache/"
+const (
+	defaultBasePath = "/_gocache/"
+	defaultReplicas = 50
+)
 
 // HTTPpoll implements peerpicker for a pool of HTTP peers.
 type HTTPPool struct {
 	// this peer base URL, eg. "https://example.net:8080"
-	self     string
-	basePath string
+	self        string
+	basePath    string
+	mu          sync.Mutex             // guards peers and httpGetters
+	peers       *consistenthash.Map    // 用来根据具体的key选择节点
+	httpGetters map[string]*httpGetter // keyed by eg "http://10.0.0.2:8008"
+	// 每一个远程节点对应一个httpGetter,因为httpGetter与远程结点的地址baseURL有关
 }
 
 // NewHTTPPool initialize an HTTP pool of peers.
@@ -61,3 +72,58 @@ func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/octet-stream")
 	w.Write(view.ByteSlice())
 }
+
+type httpGetter struct {
+	baseURL string // baseURL表示将要访问的远程结点的地址，例如 http://example.com/_geecache/
+}
+
+func (h *httpGetter) Get(group string, key string) ([]byte, error) {
+	u := fmt.Sprintf("%v%v/%v", h.baseURL, url.QueryEscape(group), url.QueryEscape(key))
+
+	res, err := http.Get(u)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("server returned: %v", res.Status)
+	}
+
+	bytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("reading response body: %v", err)
+	}
+
+	return bytes, nil
+}
+
+var _ PeerGetter = (*httpGetter)(nil)
+
+// Set updates the pool's list of peers
+func (p *HTTPPool) Set(peers ...string) {
+	// Set方法实例化了一致性哈希算法，并且添加了传入的节点
+	// 并为每个节点创建了一个HTTP客户端httpGetter
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.peers = consistenthash.New(defaultReplicas, nil)
+	p.peers.Add(peers...)
+	p.httpGetters = make(map[string]*httpGetter, len(peers))
+	for _, peer := range peers {
+		p.httpGetters[peer] = &httpGetter{baseURL: peer + p.basePath}
+	}
+}
+
+// PickPeer picks a peer according to key
+func (p *HTTPPool) PickPeer(key string) (PeerGetter, bool) {
+	// PickPeer()包装了一致哈希算法的Get()方法，根据key选择节点，返回节点对应的HTTP客户端
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	if peer := p.peers.Get(key); peer != "" && peer != p.self {
+		p.Log("Pick peer $s", peer)
+		return p.httpGetters[peer], true
+	}
+	return nil, false
+}
+
+var _ PeerPicker = (*HTTPPool)(nil)
